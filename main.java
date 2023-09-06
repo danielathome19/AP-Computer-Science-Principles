870ae5d8eaab4f9ea5e4fbf1886615166b0812cb
danielathome19 <dszelogowski@gmail.com>
2023-09-06 14:57:04 +0000

    Update

---
+++ main.java
@@ -0,0 +1,6 @@
+// Run with "java main.java" in the shell
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
   deps = [
+    pkgs.adoptopenjdk-bin
     pkgs.python39Full
     pkgs.ruby_3_1
     pkgs.rubyPackages_3_1.solargraph
