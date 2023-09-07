29c7ead8edcfae7bcb68f50881ce2a0511a82851
danielathome19 <dszelogowski@gmail.com>
2023-09-07 03:37:21 +0000

    Update -- added working examples of all programming languages with compiler instructions

--- Hello/hello.cs
+++ Hello/hello.cs
@@ -1,5 +1,16 @@
-class hello {
+/* 
+First, run "dotnet tool install -g dotnet-script" in the shell,
+followed by "export PATH="$PATH:/home/runner/.dotnet/tools",
+followed by "export DOTNET_ROOT=$(dirname $(readlink -f $(which dotnet)))"
+*/
+// Run with "dotnet script Hello/hello.cs"
+Console.WriteLine("Hello, world!");
+
+/* Same as: 
+class Hello {
   public static void Main(string[] args) {
     Console.WriteLine("Hello, world!");
   }
 }
+Hello.Main(null);
+*/
