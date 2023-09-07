c6217fd5ef6f4ba6a41a7e66094b08ed57a4fcde
Daniel Szelogowski <dszelogowski@gmail.com>
2023-09-07 09:07:43 -0500

    Update hello.cs

--- Hello/hello.cs
+++ Hello/hello.cs
@@ -1,6 +1,6 @@
 /* 
 First, run "dotnet tool install -g dotnet-script" in the shell,
-followed by "export PATH="$PATH:/home/runner/.dotnet/tools",
+followed by "export PATH="$PATH:/home/runner/.dotnet/tools"",
 followed by "export DOTNET_ROOT=$(dirname $(readlink -f $(which dotnet)))"
 */
 // Run with "dotnet script Hello/hello.cs"
