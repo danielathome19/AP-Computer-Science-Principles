ec246b914fef4d2f723f2c26b50a8f3bfc593954
danielathome19 <dszelogowski@gmail.com>
2023-10-10 14:22:13 +0000

    Update 10/10

---
+++ bigarray.py
@@ -0,0 +1,23 @@
+import random
+lst = [random.randint(20, 91) for _ in range(19)]
+# 1. Print the Array from the beginning to the end
+for i in range(1, len(lst)):
+  print(lst[i])
+  
+# 2. Print the Array from the beginning to the end using a for-each loop
+# 3. What number is in the middle of the Array?
+# 4. What is the average of the first, last and middle numbers?
+# 5. Find the smallest and the largest number in the Array
+# 6. Switch the largest with smallest number. Print the list
+# 7. Create a new random from 1 to 10 and insert it in the middle slot. Print the numbers.
+# 8. Add 10 to every number in the List. Print all.
+# 9. Replace the 3rd element in the array with 5 and print the number that was ousted (only use one method to complete this.)
+# 10. What numbers are in the 50s?
+# 11. What numbers are multiples of 4?
+# 12. Is there a 60 in the list?
+# 13. Check to see if all of the elements from front to back are in the same order from back to front
+# 14. How many numbers are greater than the average?
+# 15. How many of the numbers are even?
+# 16. Copy all of the elements of the array into a new array but in reverse order
+# 17. Write a program to shift every element of an array circularly right. E.g.-INPUT : 1 2 3 4 5 OUTPUT : 5 1 2 3 4
+# 18. Find the sum of all of the digits of all of the elements
