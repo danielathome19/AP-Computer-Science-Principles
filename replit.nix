9c4943076291aaaeb4741b0cc62e9ecfc9edf1c5
danielathome19 <dszelogowski@gmail.com>
2023-10-16 16:10:06 +0000

    Update 10/16
    Rebuilt replit repo to fix pip

---
+++ replit.nix
@@ -0,0 +1,18 @@
+{ pkgs }: {
+  deps = [
+    pkgs.zig
+    pkgs.go
+    pkgs.nodejs
+    pkgs.swift
+    pkgs.rustup
+    pkgs.lua
+    pkgs.kotlin
+    pkgs.scala
+    pkgs.dart_stable
+    pkgs.julia-stable-bin
+    pkgs.dotnet-sdk
+    pkgs.R
+    pkgs.adoptopenjdk-bin
+    pkgs.ruby_3_1
+  ];
+}
