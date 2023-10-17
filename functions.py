1a478108c9440d6412c39aa25da0b7838fcfd377
danielathome19 <dszelogowski@gmail.com>
2023-10-17 14:12:07 +0000

    Update 10/16

--- functions.py
+++ functions.py
@@ -5,7 +5,8 @@ import numpy as np  # pip install numpy
 
 arr = np.array([1, 2, 3])
 arr2 = np.array([6, 7, 8])
-print(arr - arr2)
+print(np.sum(arr * arr2))
+print(np.dot(arr, arr2))
 
 print(math.hypot(3, 4))
 
