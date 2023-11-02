c8149698b0c31414f7ed068838bf8884c32b7bf4
danielathome19 <dszelogowski@gmail.com>
2023-11-02 04:40:29 +0000

    Update 11/1 -- added shorthand compile/run script for C/C++/C#/Rust

---
+++ compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  printf "Usage: bash compile.sh folder/filename \n    or bash compile.sh filename\n"
+  exit 1
+fi
+
+# Uncomment whichever compiler you're using and comment out the rest
+g++ -o "$1.exe" "$1.cpp" && ./"$1.exe"
+# gcc -o "$1.exe" "$1.c" && ./"$1.exe"
+# mcs -out:"$1.exe" "$1.cs" && mono "$1.exe"
+# rustc "$1.rs" -o ./"$1.exe" && ./"$1.exe"
