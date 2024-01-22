0a9cf2818c6264e0078271f413143db7f6086efb
danielathome19 <dszelogowski@gmail.com>
2024-01-22 15:21:18 +0000

    Update 1/22

---
+++ prog985a.py
@@ -0,0 +1,15 @@
+class Calc:
+  def add(a, b):
+    return a + b
+
+  def sub(a, b):
+    return a - b
+
+  def mul(a, b):
+    return a * b
+
+  def div(a, b):
+    return a / b
+
+  def mod(a, b):
+    return a % b
---
+++ test_prog985a.py
@@ -0,0 +1,44 @@
+import random
+import unittest
+from prog985a import Calc
+from time import perf_counter as current_time
+""" Test each case with the following:
+Normal case: 1, 2
+Edge case: 0, 0
+Error case: 1, -1
+"""
+
+
+class TestCalc(unittest.TestCase):
+
+  def setUp(self):
+    self.startTime = current_time()
+
+  def tearDown(self):
+    t = current_time() - self.startTime
+    print(f"{self.id()}: {t:.6f}")
+
+  def test_add_normal_case(self):
+    self.assertEqual(Calc.add(1, 2), 3)
+
+  def test_add_edge_case(self):
+    self.assertEqual(Calc.add(0, 0), 0)
+
+  def test_add_error_case(self):
+    self.assertEqual(Calc.add(1, -1), 0)
+
+  # TODO: finish sub, mul, and mod
+
+  def test_div_normal_case(self):
+    self.assertEqual(Calc.div(1, 2), 0.5)
+
+  def test_div_edge_case(self):
+    with self.assertRaises(ZeroDivisionError):
+      Calc.div(0, 0)
+
+  def test_div_error_case(self):
+    self.assertEqual(Calc.div(1, -1), -1)
+
+if __name__ == '__main__':
+  suite = unittest.TestLoader().loadTestsFromTestCase(TestCalc)
+  unittest.TextTestRunner(verbosity=0).run(suite)
