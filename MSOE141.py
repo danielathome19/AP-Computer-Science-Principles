23cdd3c1cc00407f2b7f999ba2180fe79173903f
Daniel J. Szelogowski <dszelogowski@gmail.com>
2024-09-23 15:28:20 +0000

    Update 9/23

---
+++ MSOE141.py
@@ -0,0 +1,13 @@
+def main():  # run with 'python MSOE141.py' in the shell
+    word = input("Enter a word: ")
+    word = word.lower()
+    dblcount = 0
+    for lcv in range(0, len(word)-1):
+        if word[lcv] == word[lcv+1]:
+            dblcount += 1
+    print(f"{word} contains {dblcount} double letters")
+    pass
+
+
+if __name__ == "__main__":
+    main()
