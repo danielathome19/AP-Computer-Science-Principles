196e42a1bd6222b24aa54735e7e11c9bdc90a65c
Daniel James Szelogowski <dszelogowski@gmail.com>
2025-01-27 10:14:21 -0600

    Update 1/27

---
+++ prog985a.py
@@ -0,0 +1,20 @@
+class Calc:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    @staticmethod
+    def div(a, b):
+        return a / b
+
+    @staticmethod
+    def mod(a, b):
+        return a % b
