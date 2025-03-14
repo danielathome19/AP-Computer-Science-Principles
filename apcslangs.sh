17f6ece3b3ec31fad07d38455966e511856f41a9
Daniel J. Szelogowski <24834480+danielathome19@users.noreply.github.com>
2025-03-14 10:25:51 -0500

    Update apcslangs.sh

--- apcslangs.sh
+++ apcslangs.sh
@@ -1,3 +1,4 @@
+# NOTE: run this in the terminal; pay attention in case there are any prompts during installation. You also may need to run this twice
 # Install all programming language compilers/interpreters for CSP
 sudo apt update -y && sudo apt-get install apt-transport-https && wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg  --dearmor -o /usr/share/keyrings/dart.gpg && echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list && sudo apt update -y && sudo apt install -y python3 default-jdk mono-complete g++ gcc ruby fsharp javascript-common openjdk-11-jdk golang scala lua5.3 swift dart rustc julia r-base perl elixir dotnet-sdk-8.0
 # Install kotlin
